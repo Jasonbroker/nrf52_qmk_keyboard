2648ac1695520d4c74271126db8a08d1c089bcbb
Jasonbroker <zhiye1@hotmail.com>
2022-10-07 19:19:47 +0800

    添加mac win支持

--- keyboards/kindlestar/aquila/keymaps/via/keymap.c
+++ keyboards/kindlestar/aquila/keymaps/via/keymap.c
@@ -77,34 +77,34 @@ extern keymap_config_t keymap_config;
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
+    _Mac,
     _FN,
-    _extra0,
-    _extra1,
+    _extra,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,           KC_0,    KC_MINS, KC_EQL,    KC_BSPC,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,           KC_0,    KC_MINS, KC_EQL,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,       KC_O,           KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,       KC_L,           KC_SCLN, KC_QUOT, KC_ENT,    _______,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,       KC_L,           KC_SCLN, KC_QUOT, KC_ENT,    RGB_TOG,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,    KC_DOT,         KC_SLSH, KC_RSFT, KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,  _______, _______, KC_RALT,    MO(_FN),        KC_RCTL, KC_LEFT, KC_DOWN,   KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,  KC_VOLU, KC_VOLD, KC_RALT,    MO(_FN),        KC_RCTL, KC_LEFT, KC_DOWN,   KC_RGHT
     ),
-    [_FN] = LAYOUT(
-        SEL_USB, ADV_ID0, ADV_ID1, ADV_ID2, ADV_ID3, ADV_ID4, ADV_ID5, SEL_RDOE,_______,    _______,        DELBNDS, _______, _______,  REC_DFU,
-        _______, BLE_RBD, _______, _______, RESET,   TESTMOD, _______, _______, _______,    CRGBRST,        CUR_MOD, RGB_SAI, RGB_SAD,  _______,
-        _______, AD_WO_L, _______, _______, _______, TOG_GAM, _______, _______, _______,    RGB_TOG,        RGB_HUI, RGB_HUD, _______,  ENT_SLP,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,        _______, _______, RGB_VAI,
-        _______, _______, _______,                   _______, _______, _______, _______,    _______,        _______, RGB_RMOD,RGB_VAD,  RGB_MOD
-    ),
-    [_extra0] = LAYOUT(
+    [_Mac] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,        _______, _______, _______,  _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,        _______, _______, _______,  _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,        _______, _______, _______,  _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,        _______, _______, _______,
-        _______, _______, _______,                   _______, _______, _______,  _______,    _______,        _______, _______, _______,  _______
+        _______, KC_LALT, KC_LGUI,                   _______, _______, _______,  _______,    _______,        _______, _______, _______,  _______
     ),
-    [_extra1] = LAYOUT(
+    [_FN] = LAYOUT(
+        SEL_USB, ADV_ID0, ADV_ID1, ADV_ID2, ADV_ID3, ADV_ID4, ADV_ID5, SEL_RDOE,_______,    _______,        DELBNDS, _______, _______,  REC_DFU,
+        _______, BLE_RBD, _______, _______, RESET,   TESTMOD, _______, _______, _______,    CRGBRST,        CUR_MOD, RGB_SAI, RGB_SAD,  _______,
+        _______, AD_WO_L, _______, _______, _______, TOG_GAM, TG(0),   TG(1),   _______,    RGB_TOG,        RGB_HUI, RGB_HUD, _______,  ENT_SLP,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,        _______, _______, RGB_VAI,
+        _______, _______, _______,                   _______, _______, _______, _______,    _______,        _______, RGB_RMOD,RGB_VAD,  RGB_MOD
+    ),
+    [_extra] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,        _______, _______, _______,  _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,        _______, _______, _______,  _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,        _______, _______, _______,  _______,
