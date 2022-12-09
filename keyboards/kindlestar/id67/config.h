4c39228ca9d99172724a4db60742abc763f812a6
Jasonbroker <zhiye1@hotmail.com>
2022-12-10 01:28:02 +0800

    feat add id67

---
+++ keyboards/kindlestar/id67/config.h
@@ -0,0 +1,106 @@
+/* Copyright 2021 @ KindleStar(https://www.KindleStar.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x6468
+#define MANUFACTURER    IDOBAO
+#define PRODUCT         ID67
+/* USB Device descriptor parameter */
+
+#define PRODUCT_ID      0x0100
+#define DEVICE_VER      0x0101
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS {A4, A3, A2, A1, A0}
+#define MATRIX_COL_PINS {A15, B3, B4, B5, B6, B7, B12, B13, B14, B15, A8, A6, A7, B0, B1}
+
+#define LED_CAPS_LOCK_PIN B8
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+#define LED_PIN_ON_STATE 0
+
+/* NKRO */
+// #define FORCE_NKRO
+
+#define RGB_DI_PIN B9
+/* RGB Matrix Configuration */
+
+#ifdef RGB_MATRIX_ENABLE
+
+#define DRIVER_LED_TOTAL 67
+
+#define START_BREATH_IDX 13
+
+#define RGB_MATRIX_STARTUP_VAL 80
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 160
+
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_LED_FLUSH_LIMIT 26
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED  // turn off effects when suspended
+#define RGB_MATRIX_STARTUP_HUE 170
+
+//#define RGB_MATRIX_SOLID_COLOR
+
+#define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#define ENABLE_RGB_MATRIX_SOLID_COLOR
+/* Disable a single effect */
+// #define DISABLE_RGB_MATRIX_ALPHAS_MODS
+// #define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+// #define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+// #define DISABLE_RGB_MATRIX_BAND_SAT
+// #define DISABLE_RGB_MATRIX_BAND_VAL
+// #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+// #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+// #define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+// #define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+// #define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+// #define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+// #define DISABLE_RGB_MATRIX_HUE_BREATHING
+// #define DISABLE_RGB_MATRIX_HUE_PENDULUM
+// #define DISABLE_RGB_MATRIX_HUE_WAVE
+
+#endif
+
+#define KEYSTAR_LED_RED_ON() {}
+#define KEYSTAR_LED_GREEN_ON() {}
+#define KEYSTAR_LED_BLUE_ON() {}
+
+#define KEYSTAR_LED_RED_OFF() {}
+#define KEYSTAR_LED_GREEN_OFF() {}
+#define KEYSTAR_LED_BLUE_OFF() {}
+
+// #undef VAL_GPIOCCRH
+// #define VAL_GPIOCCRH            0x33388888      /* PC15...PC8 */
+
+// #undef STM32_LSECLK
---
+++ keyboards/kindlestar/id67/rules.mk
@@ -0,0 +1,46 @@
+# MCU name
+MCU = STM32F103
+
+BOOTLOADER = stm32duino
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes          # Mouse keys
+EXTRAKEY_ENABLE = yes         # Audio control and System control
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no         # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes             # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no         # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+AUDIO_ENABLE = no           # Audio output
+
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+DEBOUNCE_TYPE = sym_defer_pk    # Debounce using eager_pk.
+NO_USB_STARTUP_CHECK = yes
+# CUSTOM_MATRIX = lite
+# DEFERRED_EXEC_ENABLE = yes
+
+
+# debug info
+DEBUG_ENABLE ?=no
+CONSOLE_ENABLE ?=no
+# COMMAND_ENABLE = yes           # Commands for debug and configuration
+
+#EXCLUDE = $(CHIBIOS_DIR)
+SRC += ../kindlestar_output_auto_select.c
+SRC += ../usb_sender.c
+SRC += ../uart_sender.c
+SRC += ../kindlestar_custom_animation.c
+
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+KINDLE_PATH = keyboards/kindlestar
+# OPT_DEFS += -DUSE_HAL_DRIVER
+
+# OPT_DEFS += -DSTM32F103xB  # duino 里面定义了已经
+# OPT_DEFS += -DSTM32_LSECLK=0
